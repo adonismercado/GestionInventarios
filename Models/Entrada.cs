0ce2feea4973916960b789adb08afb26659bf60b
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-01-15 18:29:03 -0500

    feat ✨: Crea modelo Entrada

---
+++ Models/Entrada.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP1GestionInventario.Models
+{
+    public class Entrada
+    {
+        [Key]
+        public int EntradaId { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El concepto es obligatorio")]
+        [StringLength(300)]
+        public string Concepto { get; set; } = string.Empty;
+
+        public decimal Total { get; set; }
+
+        public ICollection<EntradaDetalle> EntradaDetalles { get; set; } = new List<EntradaDetalle>();
+    }
+}
